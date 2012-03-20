1b7f1f61402afe7b96a4b2e2b176f815bd471046
Bryan Herman <bherman@mit.edu>
2012-03-20 14:33:48 -0400

    added more cross sections for processing

---
+++ lib/Zr_90.m
@@ -0,0 +1,25 @@
+% Zr-90 Cross Sections
+
+% run scattering
+run('./Zr_90_scat.m');
+
+% set capture and fission to zero
+xs_capt = zeros(length(xs_scat),1);
+xs_fiss = zeros(length(xs_scat),1);
+
+% size of vectors
+sizeE = length(E);
+
+% write out hdf5 file
+hdfile = 'Zr_90.h5';
+delete(hdfile);
+h5create(hdfile,'/vecsize',1);
+h5write(hdfile,'/vecsize',sizeE);
+h5create(hdfile,'/xs_scat',sizeE);
+h5write(hdfile,'/xs_scat',xs_scat);
+h5create(hdfile,'/xs_capt',sizeE);
+h5write(hdfile,'/xs_capt',xs_capt);
+h5create(hdfile,'/xs_fiss',sizeE);
+h5write(hdfile,'/xs_fiss',xs_fiss);
+h5create(hdfile,'/E_width',1);
+h5write(hdfile,'/E_width',dE);
